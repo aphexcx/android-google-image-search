5fa83e076605e7957ab0d1291db5ec88416573d4
aphex <aphex@aphex.cx>
2014-03-26 03:10:33 -0700

    Tweak Image Display Activity to try to implement sharing, but there seems to be a bug with the ShareActionProvider - the menu item isn't accepting clicks.

--- app/src/main/java/com/example/gridimagesearch/app/ImageDisplayActivity.java
+++ app/src/main/java/com/example/gridimagesearch/app/ImageDisplayActivity.java
@@ -1,14 +1,28 @@
 package com.example.gridimagesearch.app;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Environment;
 
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.widget.ShareActionProvider;
 import com.loopj.android.image.SmartImageView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-public class ImageDisplayActivity extends Activity {
+
+public class ImageDisplayActivity extends SherlockFragmentActivity {
+
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +31,83 @@ public class ImageDisplayActivity extends Activity {
         ImageResult result = (ImageResult) getIntent().getSerializableExtra("result");
         SmartImageView ivImage = (SmartImageView) findViewById(R.id.ivResult);
         ivImage.setImageUrl(result.getFullUrl());
+        setTitle(result.getTitle());
+
+////        Intent i = new Intent(this, ImageDisplayActivity.class);
+//        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+//        shareIntent.setType("image/*");
+//        Uri uri = Uri.fromFile(new File(getFilesDir(), "foo.jpg"));
+//        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+
+        // Fetch Bitmap Uri locally
+//        SmartImageView ivImage = (SmartImageView) findViewById(R.id.ivResult);
     }
 
+    private void createImageShareIntent() {
+        SmartImageView ivImage = (SmartImageView) findViewById(R.id.ivResult);
+        Uri bmpUri = getLocalBitmapUri(ivImage); // see previous section
+        // Create share intent as described above
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+        // Attach share event to the menu item provider
+        setShareIntent(shareIntent);
+    }
+
+    public Uri getLocalBitmapUri(SmartImageView imageView) {
+        Bitmap bitmap = getImageBitmap(imageView);
+        // Write image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image.png");
+            file.mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+
+    public Bitmap getImageBitmap(SmartImageView imageView) {
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp;
+        if (drawable instanceof BitmapDrawable) {
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else { // workaround to convert color to bitmap
+            bmp = Bitmap.createBitmap(drawable.getBounds().width(),
+                    drawable.getBounds().height(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bmp);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+        }
+        return bmp;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.image_display, menu);
+        getSupportMenuInflater().inflate(R.menu.image_display, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
         return true;
+    }
+
+    // Call to update the share intent
+    private void setShareIntent(Intent shareIntent) {
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
     }
 
     @Override
@@ -34,7 +116,8 @@ public class ImageDisplayActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.menu_item_share) {
+            createImageShareIntent();
             return true;
         }
         return super.onOptionsItemSelected(item);
